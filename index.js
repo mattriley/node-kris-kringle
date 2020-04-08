997352bb6be929480528a4e74fdd44f0913d4098
Matthew Riley <matthew@matthewriley.name>
2020-04-09 09:07:49 +1000

    use module-initialiser

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const minit = require('@mattriley/minit');
+const { init } = require('@mattriley/module-initialiser');
 const src = require('./src');
 
 module.exports = ({ fs, sns }) => {
-    const lib = minit(src.lib, { fs, sns });
-    const commands = minit(src.commands, { lib });
+    const lib = init(src.lib, { fs, sns });
+    const commands = init(src.commands, { lib });
     return commands;
 };
