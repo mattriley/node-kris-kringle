c1d8acb5142f67fe2ef5a925667b0c4b75d48911
Matt Riley <mattrileyaus@gmail.com>
2020-07-23 23:27:19 +1000

    extract io module

--- bin/kk.js
+++ bin/kk.js
@@ -1,9 +1,5 @@
-const AWS = require('aws-sdk');
-const sns = new AWS.SNS({ region: 'ap-southeast-2' });
-const fs = require('fs');
-
 const initialise = require('../src/initialise');
-const { commands } = initialise({ fs, sns });
+const { commands } = initialise();
 
 const dataFile = process.argv[2];
 const data = JSON.parse(fs.readFileSync(dataFile));
