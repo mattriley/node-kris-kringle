086c489e40d4a750464fe16b627c339630c9b826
Matt Riley <mattrileyaus@gmail.com>
2021-12-01 13:15:44 +1100

    write config to output file

--- src/service/write-result-file.js
+++ src/service/write-result-file.js
@@ -4,7 +4,7 @@ module.exports = ({ io, config }) => async p => {
 
     const players = await p;
     const now = io.getDate();
-    const data = { timestamp: now.toISOString(), players };
+    const data = { timestamp: now.toISOString(), config, players };
     const json = JSON.stringify(data, null, 4);
     const outputFile = path.join(config.outputDir, `${now.getTime()}.json`);
     await io.fsp.mkdir(config.outputDir, { recursive: true });
