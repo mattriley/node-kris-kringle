00be6066ad32f9a92478ca4060541d7bfa58896b
Matt Riley <mattrileyaus@gmail.com>
2022-05-02 10:21:32 +1000

    Unspecified changes

--- src/compose.js
+++ src/compose.js
@@ -1,9 +1,10 @@
 const composer = require('module-composer');
 const modules = require('./modules');
+const defaultConfig = require('./default-config');
 
-module.exports = ({ config }) => {
+module.exports = (...configs) => {
 
-    const compose = composer(modules);
+    const { compose, config } = composer(modules, defaultConfig, ...configs);
     const { io } = compose('io', { config }, io => io.setup());
     const { services } = compose('services', { io, config });
     return compose('commands', { services, io, config });
